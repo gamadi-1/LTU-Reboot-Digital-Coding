732cd21ed59af7bbc2ac634eb310e80cfeac0e1a
Gillian-Stephen <65459988+Gillian-Stephen@users.noreply.github.com>
2021-11-11 17:16:56 -0400

    Learning about commenting and creating variables

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,22 @@
+console.log('Hello');
+
+//Single line comment
+
+/*Learning about commenting
+
+Multi line comment
+
+/*This time commenting multiple lines
+here*/
+
+let productName = 'lightbulb';
+let price = '6.00';
+let quantity = 2;
+let inStock = true;
+let discountAmount = 4;
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountAmount);
