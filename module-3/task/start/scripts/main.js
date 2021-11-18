840f5c7f5d76a413f66f74b2641cbc78c5260fce
Gillian-Stephen <65459988+Gillian-Stephen@users.noreply.github.com>
2021-11-18 17:01:59 -0400

    Created a calculator

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -78,3 +78,104 @@ function productDiscount(){
 //     }
 //     console.log(sum);
 
+
+
+//FUNCTION called drinkOrder that takes 2 parameters {Size} & {drink}
+ function drinkOrder(size, drink){   
+    // VALIDATE the variables passed through
+    if(size !='small' && size != 'medium' && size != 'large'){
+        console.log('You have ordered a size we don\'t sell');
+        return;
+    }
+    let message = 'You have ordered a ' + size;
+    // SWITCH {drink}
+    switch(drink){
+        case 'cola':
+            message += ' Coca-Cola';
+            break;
+        case 'lemon':
+            message += ' Schwepps';
+            break;
+        case 'orange':
+            message += ' Tango';
+            break;
+        default:
+            message = 'You have ordered a drink we don\'t sell';
+            break;
+    }
+    // RETURN message
+    console.log(message);
+ }
+   
+drinkOrder('grande');
+drinkOrder('small', 'cola');
+drinkOrder('medium','orange');
+drinkOrder('grnade', 'tango');
+
+// ## Task 9
+
+// 1. Create a function called calculator, which takes in 3 parameters, num1, num2, operator
+// 2. Inside the function use a switch statement taking in the operator as a parameter
+// 3. Create 4 cases and 1 default case
+// 4. Each case should be for addition (+), subtraction (-),division (/) and multiplication (`*`)
+// 5. Each case should console.log the result, for example num1 + num2, num1 / num2, etc
+// 6. Call the function
+
+// - calculator( 1, 2, '+');
+// - calculator( 1, 2, '/');
+
+//FUNCTION calculator {num1} {num2} {operator}
+function calculator(num1,num2,operator){  
+    //VALIDATE ALL DATA
+    if(isNaNValidator(num1)){
+        console.log('Number 1 is not a number');
+        return;
+    }
+    if(isNaNValidator(num2)){
+        console.log('Number 2 is not a number');
+        return;
+    }
+    //SWITCH {operator}
+    let sum;
+    switch(operator){
+        case '+' :
+            sum = num1 + num2;
+            break;
+        case '_':
+            sum = num1 - num2;
+            break;
+        case '/':
+            sum = num1 / num2;
+            break;
+        case '*':
+        case 'x':
+            sum = num1 * num2;
+            break;
+        case '%':
+            sum = num1 % num2;
+            break;
+        default:
+            return'Operator is not recognised';
+    }
+    return sum;
+}
+
+function isNaNValidator(number) {
+    return isNaN(number);
+    console.log('Number 1 is not a number');
+
+}
+
+calculator(1,'alex');
+calculator('alex');
+calculator(1,1,'alex');
+let sum1 = calculator(10,2,'+');
+let sum2 = calculator(10,2,'-');
+let sum3 = calculator(10,2,'/');
+let sum4 = calculator(10,2,'*');
+let sum5 = calculator(10,2,'%');
+console.log(sum1);
+console.log(sum2);
+console.log(sum3);
+console.log(sum4);
+console.log(sum5);
